910c46f55fd8a87746978d2fc70a5ae20072f02a
허영훈 <huny4daniel@gmail.com>
2026-04-14 17:54:30 +0900

    StudyLog (2026.04.14.)

---
+++ 260409/ex6-8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int add(int a, int &b) {
+    b = b + a;
+    return b;
+}
+
+int main() {
+    int s=10, t=20;
+}
